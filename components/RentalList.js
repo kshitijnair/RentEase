6c1416138206e81811fe703f99a505d982e9fbf7
SujitNasahik <sujitnashik.nashik4@gmail.com>
2023-03-28 23:53:53 -0700

    list item and search button completed

--- components/RentalList.js
+++ components/RentalList.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { firestore } from '../firebase/firebaseSetup';
+import { collection, query, onSnapshot } from 'firebase/firestore';
+
+import RentalListItem from './RentalListItem';
 
 const RentalList = () => {
+  const [listings, setListings] = useState([]);
+
+
+  useEffect(() => {
+    const q = query(collection(firestore, 'Listing'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const listings = [];
+      querySnapshot.forEach((doc) => {
+        listings.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setListings(listings);
+      console.log(listings);
+    });
+
+    return unsubscribe;
+  }, []);
   return (
-    <View>
-      <Text>RentalList</Text>
+    <View style={styles.container}>
+      <RentalListItem data={listings} />
     </View>
-  )
-}
+  );
+};
 
-export default RentalList
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default RentalList;

82ce9504bd5caf2bce3a3cabee56c376d04deb37
unknown <kshitijnairr@gmail.com>
2023-03-31 22:57:28 -0700

    added profile.js

--- components/TestComponent.js
+++ components/TestComponent.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import RentalList from "./RentalList";
 
 const TestComponent = () => {
   return (
-    <View>
-      <Text>TestComponent</Text>
+    <View style={styles.container}>
+      <RentalList />
+      <StatusBar style="auto" />
     </View>
-  )
-}
+  );
+};
 
-export default TestComponent
+export default TestComponent;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+});

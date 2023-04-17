3f7fcd24538ee6c98a59757607f436634b721a29
kshitijnair <kshitijnairr@gmail.com>
2023-04-17 12:36:53 -0700

    changed login screend esign

--- components/Comment.js
+++ components/Comment.js
@@ -14,7 +14,6 @@ const Comment = ({ listingID, modalVisible, setModalVisible }) => {
 
   return (
     <Modal
-      style={styles.modal}
       animationType="slide"
       visible={modalVisible}
       onRequestClose={() => {
@@ -23,23 +22,32 @@ const Comment = ({ listingID, modalVisible, setModalVisible }) => {
       }}
     >
       <SafeAreaView style={styles.container}>
-        <Text>Leave a comment:</Text>
-        <TextInput
-          placeholder="enter comment here "
-          onChangeText={(comment) => setComment(comment)}
-        />
-        <Text>Rate your visit:</Text>
-        <TextInput
-          keyboardType="numeric"
-          maxLength={1}
-          onChangeText={(rating) => setRating(rating)}
-        />
-      </SafeAreaView>
-      <View style={styles.buttonContainer}>
-        <Button title="Cancel" onPress={removeModal} />
+        <Text style={styles.header}>Feedback</Text>
 
-        <Button title="Submit" />
-      </View>
+        <View style={styles.innerContainer}>
+          <View>
+            <Text style={styles.label}>Leave a comment</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="enter comment here "
+              onChangeText={(comment) => setComment(comment)}
+            />
+          </View>
+          <View>
+            <Text style={styles.label}>Rate your visit</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              maxLength={1}
+              onChangeText={(rating) => setRating(rating)}
+            />
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Cancel" onPress={removeModal} />
+          <Button title="Submit" />
+        </View>
+      </SafeAreaView>
     </Modal>
   );
 };
@@ -48,19 +56,39 @@ export default Comment;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 3,
-    height: 300,
-    backgroundColor: "#eee",
-    // alignItems: "center",
+    flex: 1,
     justifyContent: "center",
+    alignItems: 'center'
   },
-  modal: {
-    width: 200,
-    height: 200,
+  innerContainer: {
+    // backgroundColor: 'rgba(0,0,0,0.1)'
+    flex: 1,
+    borderColor: "red",
+    borderWidth: 2,
+    justifyContent: "center",
+    width: 300
+  },
+  header: {
+    textAlign: "center",
+    marginTop: 50,
+    marginBottom: 50,
+    fontSize: 24,
+    fontWeight: 400,
+  },
+  label: {
+    padding: 5,
+    fontSize: 16,
+  },
+  input: {
+    borderBottomColor: "pink",
+    borderTopColor: 'rgba(0,0,0,0)',
+    borderLeftColor: 'rgba(0,0,0,0)',
+    borderRightColor: 'rgba(0,0,0,0)',
+    borderWidth: 1,
   },
   buttonContainer: {
-    flex: 2,
+    flex: 1,
     flexDirection: "row",
-    justifyContent: "space-evenly",
+    justifyContent: 'space-around',
   },
 });
--- components/Login.js
+++ components/Login.js
@@ -5,14 +5,17 @@ import {
   SafeAreaView,
   Button,
   Alert,
+  Image,
+  View,
 } from "react-native";
 import React, { useState } from "react";
+
 import { auth } from "../firebase/firebaseSetup";
 import {
   getUserDetails,
   loginWithEmailAndPassword,
 } from "../firebase/firebaseHelper";
-import {  signInAnonymously } from 'firebase/auth';
+import { signInAnonymously } from "firebase/auth";
 
 const Login = ({ navigation, setUserHasProfile }) => {
   const [userEmail, setUserEmail] = useState("");
@@ -36,7 +39,7 @@ const Login = ({ navigation, setUserHasProfile }) => {
           Alert.alert(`Welcome, ${userDetails["name"]}!`);
         } else {
           setUserHasProfile(false);
-          console.log("in else")
+          console.log("in else");
         }
       }
     } else Alert.alert("Please check input fields!");
@@ -56,34 +59,86 @@ const Login = ({ navigation, setUserHasProfile }) => {
   };
 
   return (
-    <SafeAreaView>
-      <Text style={styles.banner}>Hi!</Text>
-      <TextInput
-        style={styles.inputField}
-        value={userEmail}
-        placeholder="email"
-        onChangeText={(newEmail) => {
-          console.log(newEmail);
-          setUserEmail(newEmail);
+    <SafeAreaView style={styles.mainView}>
+      <View
+        style={{
+          flex: 2,
+          justifyContent: "center",
         }}
-      />
-      <TextInput
-        style={styles.inputField}
-        secureTextEntry={true}
-        value={userPassword}
-        placeholder="p**sw**d"
-        onChangeText={(newPassword) => {
-          console.log("password updated");
-          setUserPassword(newPassword);
-        }}
-      />
-      <Button title="Sign In" onPress={signInHander} />
-      <Button title="Sign Up" onPress={signUpHandler} />
-      <Button title="Sign In Anonymously" onPress={signInAnonymouslyHandler} />
+      >
+        <Image source={require("../assets/logo.png")} style={styles.logo} />
+      </View>
+      <View style={{ flex: 1}}>
+        <Text style={styles.banner}>Welcome to your new house!</Text>
+      </View>
+      <View
+        style={[
+          styles.inputContainer,
+          { flex: 1.5 },
+        ]}
+      >
+        <TextInput
+          style={styles.inputField}
+          value={userEmail}
+          placeholder="email"
+          onChangeText={(newEmail) => {
+            console.log(newEmail);
+            setUserEmail(newEmail);
+          }}
+        />
+        <TextInput
+          style={styles.inputField}
+          secureTextEntry={true}
+          value={userPassword}
+          placeholder="p**sw**d"
+          onChangeText={(newPassword) => {
+            console.log("password updated");
+            setUserPassword(newPassword);
+          }}
+        />
+      </View>
+      <View style={{ flex: 2 }}>
+        <Button title="Sign In" onPress={signInHander} />
+        <Button title="Sign Up" onPress={signUpHandler} />
+        <Button
+          title="Sign In Anonymously"
+          onPress={signInAnonymouslyHandler}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 110,
+    height: 110,
+    // margin: 20,
+    // top: 75,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  banner: {
+    fontSize: 32,
+    width: 225,
+    justifyContent: "space-evenly",
+    paddingBottom: 5
+  },
+  inputContainer: {
+    width: 220,
+    height: 100,
+  },
+  inputField: {
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    borderRadius: 15,
+    padding: 15,
+    marginTop: 10,
+  }
+});
